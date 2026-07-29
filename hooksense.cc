9efc2ad745c18867e5596dfeed82f586619b6dd3
xqmt <btng2043@gmail.com>
2026-07-30 06:38:41 +0700

    Reduce FOVCircleOutline thickness from 1.2 to 1.0

--- hooksense.cc
+++ hooksense.cc
@@ -575,7 +575,7 @@ BoxFillGui.Parent = TargetGuiParent
 
 -- [External-like Premium FOV Draw]
 local FOVCircleOutline = Drawing.new("Circle")
-FOVCircleOutline.Thickness = 1.2
+FOVCircleOutline.Thickness = 1.0
 FOVCircleOutline.NumSides = 144
 FOVCircleOutline.Filled = false
 FOVCircleOutline.Visible = FOVVisible
